bd3ce94b3538deab4728e1293ad27c475403e34a
Pablo Fajardo <pabloandresfajardo18@gmail.com>
2021-08-11 08:12:09 -0500

    nuevo cambio

---
+++ programas/enums.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+ 
+ enum decks {
+     club=0,
+     diamonds=5,
+     hearts=10,
+     spades=15
+     }card;
+
+ int main()
+ {
+ 
+ 
+card;
+ card= diamonds;
+ printf ("Size of var %d",sizeof (card));
+ return 0;
+ }
---
+++ programas/helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    /*dddddddddddddddddddddddddddd*/
+printf ("Hello moto ");
+return 0;
+}
